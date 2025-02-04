e0922eac586f5a2727920c286acd0ed8945e9e96
Akalak Kruaboon <135732879+aklkbqx@users.noreply.github.com>
2025-02-04 17:44:07 +0700

    Add files via upload

--- elderly_db.sql
+++ elderly_db.sql
@@ -1,3 +1,32 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: mariadb
+-- Generation Time: Feb 04, 2025 at 10:42 AM
+-- Server version: 11.6.2-MariaDB-ubu2404
+-- PHP Version: 8.2.27
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `elderly_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `assessments`
+--
+
 CREATE TABLE `assessments` (
   `assessment_id` int(11) NOT NULL,
   `title` varchar(255) NOT NULL,
@@ -9,6 +38,19 @@ CREATE TABLE `assessments` (
   `updated_at` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
 
+--
+-- Dumping data for table `assessments`
+--
+
+INSERT INTO `assessments` (`assessment_id`, `title`, `body`, `questions`, `additional`, `visitors`, `created_at`, `updated_at`) VALUES
+(2, 'รีวิว!!!!', 'ความพอใจ', '[\"\\u0e27\\u0e34\\u0e27\\u0e17\\u0e35\\u0e48\\u0e2a\\u0e27\\u0e22\\u0e07\\u0e32\\u0e21\",\"\\u0e2d\\u0e32\\u0e01\\u0e32\\u0e28\\u0e17\\u0e35\\u0e48\\u0e14\\u0e35!!!!!!!!\",\"\\u0e42\\u0e14\\u0e22\\u0e23\\u0e27\\u0e21\"]', 'มีอะไรอยากจะแนะนำฉันไหม', 0, '2025-02-03 09:07:03', '2025-02-04 07:41:01');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `assessment_responses`
+--
+
 CREATE TABLE `assessment_responses` (
   `assessment_id` int(11) NOT NULL,
   `user_id` int(11) NOT NULL,
@@ -17,6 +59,12 @@ CREATE TABLE `assessment_responses` (
   `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
   `updated_at` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bookings`
+--
 
 CREATE TABLE `bookings` (
   `booking_id` int(11) NOT NULL,
@@ -32,6 +80,12 @@ CREATE TABLE `bookings` (
   `updated_at` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bot_responses`
+--
+
 CREATE TABLE `bot_responses` (
   `response_id` int(11) NOT NULL,
   `questions` varchar(255) NOT NULL,
@@ -40,14 +94,57 @@ CREATE TABLE `bot_responses` (
   `updated_at` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `chats`
+--
+
 CREATE TABLE `chats` (
   `chat_id` int(11) NOT NULL,
   `sender_id` int(11) NOT NULL,
   `receiver_id` int(11) NOT NULL,
   `message` varchar(150) NOT NULL,
-  `image` varchar(255) NULL DEFAULT NULL,
+  `image` varchar(255) DEFAULT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
+
+--
+-- Dumping data for table `chats`
+--
+
+INSERT INTO `chats` (`chat_id`, `sender_id`, `receiver_id`, `message`, `image`, `created_at`) VALUES
+(62, 5, 4, 'สวัสดร', NULL, '2025-02-04 10:22:30'),
+(63, 5, 4, 'สวัสดี', NULL, '2025-02-04 10:22:32'),
+(64, 5, 4, 'สวัสดี', NULL, '2025-02-04 10:22:34'),
+(65, 5, 4, 'ว่าไงงง', NULL, '2025-02-04 10:22:40'),
+(66, 4, 5, 'asdasd', NULL, '2025-02-04 10:22:56'),
+(67, 4, 5, 'asd', NULL, '2025-02-04 10:22:57'),
+(68, 4, 5, 'asd', NULL, '2025-02-04 10:22:57'),
+(69, 4, 5, 'asd', NULL, '2025-02-04 10:22:58'),
+(70, 4, 5, 'asd', NULL, '2025-02-04 10:22:58'),
+(71, 4, 5, 'ddddd', NULL, '2025-02-04 10:22:59'),
+(72, 4, 5, 'adasdasd', NULL, '2025-02-04 10:23:00'),
+(73, 4, 5, 'asdasdasd', NULL, '2025-02-04 10:23:01'),
+(74, 4, 5, 'asdasd', NULL, '2025-02-04 10:23:01'),
+(75, 4, 5, 'asdasd', NULL, '2025-02-04 10:23:02'),
+(76, 4, 5, 'asd', NULL, '2025-02-04 10:23:02'),
+(77, 4, 5, 'asdasd', NULL, '2025-02-04 10:23:03'),
+(78, 5, 4, 'asdasasd', NULL, '2025-02-04 10:23:06'),
+(79, 5, 4, 'asdasd', NULL, '2025-02-04 10:23:07'),
+(80, 5, 4, 'asdasd', NULL, '2025-02-04 10:23:07'),
+(81, 4, 5, 'asdasd', NULL, '2025-02-04 10:23:10'),
+(82, 4, 5, 'asdasd', NULL, '2025-02-04 10:23:11'),
+(83, 4, 5, 'asdasd', NULL, '2025-02-04 10:23:11'),
+(84, 5, 4, 'dasdasdasd', NULL, '2025-02-04 10:23:15'),
+(85, 5, 4, 'asdasd', NULL, '2025-02-04 10:23:15'),
+(86, 5, 4, 'asdasd', NULL, '2025-02-04 10:23:16');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `news`
+--
 
 CREATE TABLE `news` (
   `news_id` int(11) NOT NULL,
@@ -59,6 +156,21 @@ CREATE TABLE `news` (
   `updated_at` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
 
+--
+-- Dumping data for table `news`
+--
+
+INSERT INTO `news` (`news_id`, `title`, `body`, `image`, `visitors`, `created_at`, `updated_at`) VALUES
+(2, 'อากาศ', 'วันนี้อากาศดีมาก', '67a0866d7f40f.jpg', 0, '2025-02-03 09:03:41', NULL),
+(3, 'วิว', 'วิวยอดนิยม', '67a1c3616b0c0.png', 0, '2025-02-03 09:05:09', '2025-02-04 07:36:01'),
+(4, 'วิว', '', '67a08d61b7c85.jpg', 0, '2025-02-03 09:33:21', '2025-02-04 07:39:58');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `news_comments`
+--
+
 CREATE TABLE `news_comments` (
   `comment_id` int(11) NOT NULL,
   `news_id` int(11) NOT NULL,
@@ -66,6 +178,12 @@ CREATE TABLE `news_comments` (
   `comment` tinyint(1) NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `news_likes`
+--
 
 CREATE TABLE `news_likes` (
   `like_id` int(11) NOT NULL,
@@ -75,6 +193,11 @@ CREATE TABLE `news_likes` (
   `created_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `places`
+--
 
 CREATE TABLE `places` (
   `place_id` int(11) NOT NULL,
@@ -87,6 +210,12 @@ CREATE TABLE `places` (
   `updated_at` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `place_reviews`
+--
+
 CREATE TABLE `place_reviews` (
   `review_id` int(11) NOT NULL,
   `place_id` int(11) NOT NULL,
@@ -97,6 +226,12 @@ CREATE TABLE `place_reviews` (
   `updated_at` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `posts`
+--
+
 CREATE TABLE `posts` (
   `post_id` int(11) NOT NULL,
   `user_id` int(11) NOT NULL,
@@ -106,6 +241,11 @@ CREATE TABLE `posts` (
   `updated_at` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `post_comments`
+--
 
 CREATE TABLE `post_comments` (
   `comment_id` int(11) NOT NULL,
@@ -116,6 +256,12 @@ CREATE TABLE `post_comments` (
   `updated_at` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `post_likes`
+--
+
 CREATE TABLE `post_likes` (
   `comment_id` int(11) NOT NULL,
   `post_id` int(11) NOT NULL,
@@ -124,6 +270,12 @@ CREATE TABLE `post_likes` (
   `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
   `updated_at` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
 
 CREATE TABLE `users` (
   `user_id` int(11) NOT NULL,
@@ -138,11 +290,21 @@ CREATE TABLE `users` (
   `updated_at` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
 
+--
+-- Dumping data for table `users`
+--
+
 INSERT INTO `users` (`user_id`, `firstname`, `lastname`, `email`, `password`, `image`, `role`, `active_status`, `created_at`, `updated_at`) VALUES
-(4, 'asd', 'asd', 'asd@gmail.com', '$2y$10$d3i02j3EFCTFiLalmgmWIu0r7pRhZe04USrHdasjdhPnMxbYlJTuq', '67a0911d1caba.jpg', 'user', 'online', '2025-01-25 08:13:01', '2025-02-03 09:49:23'),
-(5, 'admin', 'admin', 'admin@admin.com', '$2y$10$c19VUioqWilX2ukGXCtec.6cWoEDpjU9YWZV629bdit4PUWCjYFHS', 'default-profile.png', 'admin', 'online', '2025-01-25 10:21:32', '2025-02-03 08:36:29'),
+(4, 'asd', 'asd', 'asd@gmail.com', '$2y$10$W2EN3tgzp5c1DdWGILoHxOEoIwyD1/TGf7HnB2m4LCeeldSUctvxO', '67a1d47767967.png', 'user', 'online', '2025-01-25 08:13:01', '2025-02-04 08:48:55'),
+(5, 'admin', 'admin', 'admin@admin.com', '$2y$10$c19VUioqWilX2ukGXCtec.6cWoEDpjU9YWZV629bdit4PUWCjYFHS', 'default-profile.png', 'admin', 'offline', '2025-01-25 10:21:32', '2025-02-04 10:32:50'),
 (7, '123', '123', '123@gmail.com', '$2y$10$TgSE.pD7M7jn2bo6hjK9A.bJp2lq1tgrtqgbpqzgU/tMeCBsv.5gG', 'default-profile.png', 'user', 'offline', '2025-01-27 09:03:04', '2025-01-27 09:35:11'),
 (8, 'zxc', 'zxc', 'zxc@gmail.com', '$2y$10$YA4QFVyupO6njKLUUzaRk.5vTDpl73c50jwpZkfKdh5Ltgxr.t/Ii', 'default-profile.png', 'user', 'offline', '2025-01-27 09:03:10', '2025-01-27 09:33:39');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `visitors`
+--
 
 CREATE TABLE `visitors` (
   `visitor_id` int(11) NOT NULL,
@@ -150,142 +312,276 @@ CREATE TABLE `visitors` (
   `created_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
 
-INSERT INTO `visitors` (`visitor_id`, `count`, `created_at`) VALUES
-(1, 226, '2025-01-25 03:59:57');
+--
+-- Dumping data for table `visitors`
+--
 
+INSERT INTO `visitors` (`visitor_id`, `count`, `created_at`) VALUES
+(1, 261, '2025-01-25 03:59:57');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `assessments`
+--
 ALTER TABLE `assessments`
   ADD PRIMARY KEY (`assessment_id`);
 
+--
+-- Indexes for table `assessment_responses`
+--
 ALTER TABLE `assessment_responses`
   ADD PRIMARY KEY (`assessment_id`),
   ADD KEY `user_id` (`user_id`);
 
+--
+-- Indexes for table `bookings`
+--
 ALTER TABLE `bookings`
   ADD PRIMARY KEY (`booking_id`),
   ADD KEY `user_id` (`user_id`);
 
+--
+-- Indexes for table `bot_responses`
+--
 ALTER TABLE `bot_responses`
   ADD PRIMARY KEY (`response_id`);
 
+--
+-- Indexes for table `chats`
+--
 ALTER TABLE `chats`
   ADD PRIMARY KEY (`chat_id`),
   ADD KEY `receiver_id` (`receiver_id`),
   ADD KEY `sender_id` (`sender_id`);
 
+--
+-- Indexes for table `news`
+--
 ALTER TABLE `news`
   ADD PRIMARY KEY (`news_id`);
 
+--
+-- Indexes for table `news_comments`
+--
 ALTER TABLE `news_comments`
   ADD PRIMARY KEY (`comment_id`),
   ADD KEY `news_id` (`news_id`),
   ADD KEY `user_id` (`user_id`);
 
+--
+-- Indexes for table `news_likes`
+--
 ALTER TABLE `news_likes`
   ADD PRIMARY KEY (`like_id`),
   ADD KEY `news_id` (`news_id`),
   ADD KEY `user_id` (`user_id`);
 
+--
+-- Indexes for table `places`
+--
 ALTER TABLE `places`
   ADD PRIMARY KEY (`place_id`);
 
+--
+-- Indexes for table `place_reviews`
+--
 ALTER TABLE `place_reviews`
   ADD PRIMARY KEY (`review_id`),
   ADD KEY `place_id` (`place_id`),
   ADD KEY `user_id` (`user_id`);
 
+--
+-- Indexes for table `posts`
+--
 ALTER TABLE `posts`
   ADD PRIMARY KEY (`post_id`);
 
+--
+-- Indexes for table `post_comments`
+--
 ALTER TABLE `post_comments`
   ADD PRIMARY KEY (`comment_id`),
   ADD KEY `user_id` (`user_id`),
   ADD KEY `post_id` (`post_id`);
 
+--
+-- Indexes for table `post_likes`
+--
 ALTER TABLE `post_likes`
   ADD PRIMARY KEY (`comment_id`),
   ADD KEY `post_id` (`post_id`),
   ADD KEY `user_id` (`user_id`);
 
+--
+-- Indexes for table `users`
+--
 ALTER TABLE `users`
   ADD PRIMARY KEY (`user_id`),
   ADD UNIQUE KEY `email` (`email`);
 
+--
+-- Indexes for table `visitors`
+--
 ALTER TABLE `visitors`
   ADD PRIMARY KEY (`visitor_id`);
 
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `assessments`
+--
 ALTER TABLE `assessments`
   MODIFY `assessment_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
+--
+-- AUTO_INCREMENT for table `assessment_responses`
+--
 ALTER TABLE `assessment_responses`
   MODIFY `assessment_id` int(11) NOT NULL AUTO_INCREMENT;
 
+--
+-- AUTO_INCREMENT for table `bookings`
+--
 ALTER TABLE `bookings`
   MODIFY `booking_id` int(11) NOT NULL AUTO_INCREMENT;
 
+--
+-- AUTO_INCREMENT for table `bot_responses`
+--
 ALTER TABLE `bot_responses`
   MODIFY `response_id` int(11) NOT NULL AUTO_INCREMENT;
 
+--
+-- AUTO_INCREMENT for table `chats`
+--
 ALTER TABLE `chats`
-  MODIFY `chat_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `chat_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=87;
 
+--
+-- AUTO_INCREMENT for table `news`
+--
 ALTER TABLE `news`
   MODIFY `news_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
+--
+-- AUTO_INCREMENT for table `news_comments`
+--
 ALTER TABLE `news_comments`
   MODIFY `comment_id` int(11) NOT NULL AUTO_INCREMENT;
 
+--
+-- AUTO_INCREMENT for table `news_likes`
+--
 ALTER TABLE `news_likes`
   MODIFY `like_id` int(11) NOT NULL AUTO_INCREMENT;
 
+--
+-- AUTO_INCREMENT for table `places`
+--
 ALTER TABLE `places`
   MODIFY `place_id` int(11) NOT NULL AUTO_INCREMENT;
 
+--
+-- AUTO_INCREMENT for table `place_reviews`
+--
 ALTER TABLE `place_reviews`
   MODIFY `review_id` int(11) NOT NULL AUTO_INCREMENT;
 
+--
+-- AUTO_INCREMENT for table `posts`
+--
 ALTER TABLE `posts`
   MODIFY `post_id` int(11) NOT NULL AUTO_INCREMENT;
 
+--
+-- AUTO_INCREMENT for table `post_comments`
+--
 ALTER TABLE `post_comments`
   MODIFY `comment_id` int(11) NOT NULL AUTO_INCREMENT;
 
+--
+-- AUTO_INCREMENT for table `post_likes`
+--
 ALTER TABLE `post_likes`
   MODIFY `comment_id` int(11) NOT NULL AUTO_INCREMENT;
 
+--
+-- AUTO_INCREMENT for table `users`
+--
 ALTER TABLE `users`
   MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
+--
+-- AUTO_INCREMENT for table `visitors`
+--
 ALTER TABLE `visitors`
   MODIFY `visitor_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `assessment_responses`
+--
 ALTER TABLE `assessment_responses`
   ADD CONSTRAINT `assessment_responses_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `assessment_responses_ibfk_2` FOREIGN KEY (`assessment_id`) REFERENCES `assessments` (`assessment_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
+--
+-- Constraints for table `bookings`
+--
 ALTER TABLE `bookings`
   ADD CONSTRAINT `bookings_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
+--
+-- Constraints for table `chats`
+--
 ALTER TABLE `chats`
   ADD CONSTRAINT `chats_ibfk_1` FOREIGN KEY (`receiver_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `chats_ibfk_2` FOREIGN KEY (`sender_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
+--
+-- Constraints for table `news_comments`
+--
 ALTER TABLE `news_comments`
   ADD CONSTRAINT `news_comments_ibfk_1` FOREIGN KEY (`news_id`) REFERENCES `news` (`news_id`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `news_comments_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
+--
+-- Constraints for table `news_likes`
+--
 ALTER TABLE `news_likes`
   ADD CONSTRAINT `news_likes_ibfk_1` FOREIGN KEY (`news_id`) REFERENCES `news` (`news_id`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `news_likes_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
+--
+-- Constraints for table `place_reviews`
+--
 ALTER TABLE `place_reviews`
   ADD CONSTRAINT `place_reviews_ibfk_1` FOREIGN KEY (`place_id`) REFERENCES `places` (`place_id`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `place_reviews_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
+--
+-- Constraints for table `post_comments`
+--
 ALTER TABLE `post_comments`
   ADD CONSTRAINT `post_comments_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `post_comments_ibfk_2` FOREIGN KEY (`post_id`) REFERENCES `posts` (`post_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
+--
+-- Constraints for table `post_likes`
+--
 ALTER TABLE `post_likes`
   ADD CONSTRAINT `post_likes_ibfk_1` FOREIGN KEY (`post_id`) REFERENCES `posts` (`post_id`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `post_likes_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
